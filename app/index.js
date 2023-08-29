b2342ba63c0da1fc1e1ad80b003ef44110ff099a
dapmiller <dapmiller244@gmail.com>
2023-08-29 21:39:38 +1000

    Added Web.js which houses the front end of the website. It calls the header.js and navigatioBar.js which adds a basic header and navigation bar to a local site.

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import {render} from 'react-dom'; //PAckage which enables rendering of components and elements on  the web
+import Web from './Web'
+import './index.css' // Sets some order of elements
+
+
+render (
+    <Web />,
+    document.getElementById('root')
+)
