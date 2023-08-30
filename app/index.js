82acb1150724181415e335389aea606d40ef6e51
dapmiller <dapmiller244@gmail.com>
2023-08-30 15:29:44 +1000

    Added COmponents folder to hosue website elements. Added about.tsx page for future testing

--- app/index.js
+++
@@ -1,9 +0,0 @@
-import {render} from 'react-dom'; //PAckage which enables rendering of components and elements on  the web
-import Web from './Web'
-import './index.css' // Sets some order of elements
-
-
-render (
-    <Web />,
-    document.getElementById('root')
-)
