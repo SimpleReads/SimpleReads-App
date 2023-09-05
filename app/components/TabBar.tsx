b6638fb4af30af8e459ee4c0c6b651e39d7ce285
dapmiller <dapmiller244@gmail.com>
2023-09-05 18:22:29 +1000

    Changed the name of TabBar.tsx to NavBar.tsx to better reflect its purpose. Have implement style into the funtion itself such that the tabs are inline and have a blue background that stretched across the page

---
+++ app/components/TabBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TabBar = () => {
+    return(
+        <nav>
+            <ul>
+                <li> Tab 1</li>
+                <li> Tab 2</li>
+                <li> Tab 3</li>
+            </ul>
+        </nav>
+    );
+} ;
+
+export default TabBar;
