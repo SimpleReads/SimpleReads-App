2d1c8895c4ccca58f1d8d8a7aa3e8e0137f3f302
Riley Ball <r.ball@uqconnect.edu.au>
2023-10-06 08:03:25 +1000

    admin panel buttons and input/output

--- app/components/adminStatus.tsx
+++ app/components/adminStatus.tsx
@@ -1,26 +1,44 @@
 import * as React from "react";
 import getFlask from "@/lib/flaskGrab";
+import postFlask from "@/lib/postToFlask"; // assuming you also have a postFlask function
 
-export default class AdminStausBar extends React.Component<{}, {status: string}> {
+export default class AdminStatusBar extends React.Component<{}, {status: string}> {
 
-    constructor(props: any){
+    constructor(props: any) {
         super(props);
         this.state = {
             status: "Empty"
-        }
+        };
     }
-    
-    updateText = (async(status) => {
-        let msg = getFlask(status)
-        this.setState({status: await msg})
-    });
+
+    updateText = async (status: string, data?: any) => {
+        if (data) {
+            let msg = postFlask(status, data);
+            this.setState({status: await msg});
+        } else {
+            let msg = getFlask(status);
+            this.setState({status: await msg});
+        }
+    };
 
     setOn = () => {
-        this.updateText("hello_world")
+        this.updateText("hello_world");
     }
 
     setOff = () => {
-        this.updateText("test")
+        this.updateText("test");
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const formJson = Object.fromEntries(formData.entries());
+        let text1 = formJson["text1"] as string;
+        let textEtc = formJson["textEtc"] as string;
+        let data = {"text1": text1, "textEtc": textEtc};
+        this.updateText("simplifyText", data);
+        console.log(data);
     }
 
     render(): React.ReactNode {
@@ -31,9 +49,20 @@ export default class AdminStausBar extends React.Component<{}, {status: string}>
                 <button onClick={this.setOff}>FART</button>
             </div>
             <div>
+                <form method = "post" onSubmit = {this.onSubmit}>
+                    <label>
+                        Input Text 1: <input type="text" name="text1" defaultValue="Text 1"/>
+                    </label>
+                    <label>
+                        Input Text 2: <input type="text" name="textEtc" defaultValue="TextEtc"/>
+                    </label>
+                    <button type = "submit">SEND INFO TO FLASK</button>
+                </form>
+            </div>
+            <div>
                 <h3>{this.state.status}</h3>
             </div>
             </>
-        )
+        );
     }
 }
