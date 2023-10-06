9928b09841d6bad6d1640c4a56dbc0ae6b868dd8
Fuller81 <jrf0047@gmail.com>
2023-10-06 21:40:58 +1000

    Merged all the resources into one folder

--- app/lib/client.ts
+++
@@ -1,9 +0,0 @@
-import { MongoClient } from "mongodb";
-
-const Client = async (): Promise<MongoClient> => {
-  const db = new MongoClient(process.env.DB_URI as string);
-  await db.connect();
-  return db;
-};
-
-export default Client;
