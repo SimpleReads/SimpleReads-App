e9935bdd59f78cc32e600d00883adc2754dd856b
Riley Ball <r.ball@uqconnect.edu.au>
2023-10-13 21:53:13 +1000

    converted parsePDF to api route

--- app/lib/pdfApi.ts
+++ app/lib/pdfApi.ts
@@ -1,26 +1,25 @@
 export default function readPDF(data) {
-    async function post(data) {
-        console.log("new ip: 54.212.16.183")
-      const response = await fetch("http://54.212.16.183:5000/parsePDF", {
-        method: "POST",
-        body: data,
-      });
-  
-      if (!response.ok) {
-        console.log(response.json());
-        throw new Error("AAAA");
-      }
-  
-      const result = await response.json();
-      console.log(result.message);
-      return result.message;
+  async function post(formData) {
+    console.log("pdfApi.ts");
+
+    const response = await fetch("/api/parsePDF", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.log(errorData);
+      throw new Error("AAAA");
     }
-  
-    // Taking data and turning into into a form data object so it can be accessed after
-    // the post req
-    let form = new FormData();
-    console.log(typeof(data))
-    form.append("File", data);
-    return post(form);
+
+    const result = await response.json();
+    console.log(result.message);
+    return result.message;
   }
-  
+
+  let form = new FormData();
+  console.log(typeof data);
+  form.append("File", data);
+  return post(form);
+}
