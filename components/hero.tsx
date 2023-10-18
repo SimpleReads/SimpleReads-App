42f452c03e5e02986a5f78beccfd94f72217c0d2
dapmiller <dapmiller244@gmail.com>
2023-10-18 14:25:18 +1000

    Changed the upload button to have an icon in it.

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,9 +1,29 @@
+'use client'
+
 import VideoThumb from '@/public/images/hero-image-01.jpg'
 import ModalVideo from '@/components/modal-video'
 import Link from 'next/link'
 import FileDrop from '@/components/fileDrop'
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
 
 export default function Hero() {
+
+  // Style for the upload button
+  const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+  });
+
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6 relative">
@@ -34,12 +54,14 @@ export default function Hero() {
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
                 {/** I think we will be able to use the <FileUpload> instead of <Link>*/}
-                <Link className="btn text-gray-800 bg-purple-600 hover:bg-purple-500 w-full mb-4 sm:w-auto sm:mb-0" href="/read">Upload PDF</Link>              </div>
-              {/**
-              <div data-aos="fade-up" data-aos-delay="600">
-                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
+               {/** <Link className="btn text-gray-800 bg-purple-600 hover:bg-purple-500 w-full mb-4 sm:w-auto sm:mb-0" href="/read">
+                  Upload PDF
+      </Link> */}  
+                 <Button variant="contained" startIcon={<CloudUploadIcon />} href="/read">
+                    Upload file
+                </Button>
+           
               </div>
-              */}
             </div>
           </div>
         </div>
