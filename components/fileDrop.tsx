991819814909a7e40d7af9e98e4ac61737b61b97
dapmiller <dapmiller244@gmail.com>
2023-10-19 12:07:24 +1000

    Increases font size. Make font same style and unbolded. Added filedrop icon next to 'Drag and drop a pdf file here'. Make text box slighlty bigger. Swapped Remove and Confirm buttons around. Added Spacing above and in between. Made buttons bigger with bigger text as well. Also unbolded and same fotn style.

--- components/fileDrop.tsx
+++ components/fileDrop.tsx
@@ -2,6 +2,7 @@
 import { DragEvent, useState } from 'react';
 import readPDF from '@/app/lib/pdfApi'
 import CircularProgress from '@mui/material/CircularProgress'
+import FileUploadIcon from '@mui/icons-material/FileUpload';
 import Box from '@mui/material/Box'
 import { handleDrop } from '@/app/lib/handleDrop';
 /**
@@ -11,6 +12,7 @@ import { handleDrop } from '@/app/lib/handleDrop';
  * @returns a rendered JSX Box Component that accepts dropped files
  */
 export default function FileDrop({childToParent}) {
+  // State variables to handle the components behaviour 
   const [isOver, setIsOver] = useState(false);
   const [files, setFiles] = useState<File[]>([]);
   const [uploaded, setUploaded] =useState<boolean>(false);
@@ -41,7 +43,7 @@ export default function FileDrop({childToParent}) {
       <div className = "row-span-4 col-span-4">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <section>
+            <section> {/* Place outlined box that user drops pdf onto and it registers the events of being dropped*/}
               <div
                 onDragOver={handleDragOver}
                 onDragLeave={handleDragLeave}
@@ -50,49 +52,58 @@ export default function FileDrop({childToParent}) {
                   display: 'flex',
                   justifyContent: 'center',
                   alignItems: 'center',
-                  height: '300px',
+                  height: '400px',
                   width: '800px',
                   border: '1px dotted',
                   backgroundColor: isOver ? 'lightgray' : 'white',
+                  fontSize: '35px',
+                  fontWeight:'400'
                 }}>
               
               {uploaded ? (
-                <ul>
+                <ul> {/*  Display the name of the pdf */}
                   {files.map((file, index) => (
-                    <li key={index}>{file.name}</li>
+                    <li key={index} style= {{fontSize: '25px', fontWeight:'400'}}>{file.name}</li>
                   ))} 
                 </ul>
                 ) : (
-                  <div>
+                  <div> {/* Displays "loading" while pdf is being read */}
                     {status === "LOADING" ? (
-                      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                        <CircularProgress />
-                        <p>Loading</p>
+                      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', fontSize: '35px', fontWeight:'400'}}>
+                        <CircularProgress sx={{width: '120', height: '120'}} /> <p>Loading</p>
                       </Box>
                     ) : (
-                      status
+                      <> {/* Displays the file upload icon next to "Drag and drop a pdf file here" text */}
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                          <FileUploadIcon style={{ fontSize: '40px', marginRight: '10px' }} />
+                          <span>Drag and drop a pdf file here</span>
+                        </div>
+                      </>
                     )}
                   </div>
                 )}
-                  
-                </div>
-                  <div>
-                    {uploaded && (
-                      <section>
-                        <button className="btn text-gray-800 bg-purple-600 hover:bg-purple-500 w-full mb-4 sm:w-auto sm:mb-0" 
-                          type = "button" onClick = {() => childToParent(info)}>CONFIRM</button>
-                        
-                        <button className="btn text-gray-800 bg-purple-600 hover:bg-purple-500 w-full mb-4 sm:w-auto sm:mb-0" 
-                          type = "button" onClick = {() => {setUploaded(false); setStatus("Drag and drop a pdf file here")}}>
+              </div>
+
+              <div> {/* Place two buttons "confirm and remove" that either directs page to scrollbox or to reupload pdf*/}
+                {uploaded && (
+                  <section style = {{marginTop: '10px'}}> 
+                    <div style= {{display: 'flex', justifyContent: 'space-evenly', marginBottom: '10px'}}>
+                      <button className="btn text-gray-800 bg-purple-600 hover:bg-purple-500 w-full mb-4 sm:w-auto sm:mb-0" 
+                        type = "button" style={{ fontSize: '25px', padding: '15px 25px', fontWeight:'400' }} onClick = {() => {setUploaded(false); setStatus("Drag and drop a pdf file here")}}>
                           REMOVE
-                        </button>
-                      </section>
-                    )}
-                </div>
-              </section>
-            </div>
+                      </button>
+                      <button className="btn text-gray-800 bg-purple-600 hover:bg-purple-500 w-full mb-4 sm:w-auto sm:mb-0" 
+                        type = "button" style={{ fontSize: '25px', padding: '15px 25px', fontWeight:'400' }} onClick = {() => childToParent(info)}>
+                          CONFIRM
+                      </button>     
+                    </div>        
+                  </section>
+                )}
+              </div>
+            </section>
           </div>
         </div>
+      </div>
     </>
   );
 }
