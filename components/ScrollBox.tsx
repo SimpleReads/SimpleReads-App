d27b790a82f30da1c40a501d7d3c3b3732d9cdb3
Riley Ball <r.ball@uqconnect.edu.au>
2023-10-24 01:20:20 +1000

    fixed for definitions

--- components/ScrollBox.tsx
+++ components/ScrollBox.tsx
@@ -11,20 +11,26 @@ export default function ScrollBox({ headings, text, scroll, timer, searchWord })
               <span onClick={() => { searchWord(section) }}>{`${section}`}</span>
             </h1>
             <p style={textstyle} className="scrollboxtext" id={`${section}${index}`}>
-              {text[index].split(/(\n-)/).map((chunk, i) => {
+            {text[index].split(/(\n-)/).map((chunk, i) => {
                 if (chunk.startsWith('\n-')) {
-                  return (
-                    <>
-                      <br />
-                      <span key={i} onClick={() => { searchWord(chunk) }}>{chunk.replace('\n', '')}</span>
-                    </>
-                  );
+                    return (
+                        <>
+                            <br />
+                            {chunk.split(/( )/).map((word, j) => (
+                                <span key={`${i}-${j}`} onClick={() => { searchWord(word) }}>{word}</span>
+                            ))}
+                        </>
+                    );
                 } else {
-                  return (
-                    <span key={i} onClick={() => { searchWord(chunk) }}>{chunk}</span>
-                  );
+                    return (
+                        <>
+                            {chunk.split(/( )/).map((word, j) => (
+                                <span key={`${i}-${j}`} onClick={() => { searchWord(word) }}>{word}</span>
+                            ))}
+                        </>
+                    );
                 }
-              })}
+            })}
             </p>
             <br></br>
           </div>
@@ -40,3 +46,34 @@ export default function ScrollBox({ headings, text, scroll, timer, searchWord })
     </div>
   );
 }
+
+// import {headingStyle, divstyle, textstyle} from "./style"
+
+// export default function ScrollBox({headings, text, scroll, timer, searchWord}) {
+
+//     //Generate the individual elements for all the spans and paragraphs/headers of the text
+//     const renderText = (sections, text) => {
+//         return(
+//         <>
+//             {sections.map((section, index) => (
+//             <div>
+//                 <h1 style={headingStyle} className="scrollboxtext" id={`${section}${index}Header`}>
+//                 <span onClick={() => {searchWord(section)}}>{`${section}`}</span>
+//                 </h1>
+//                 <p style={textstyle} className="scrollboxtext" id={`${section}${index}`}>
+//                 {text[index].split(/( )/).map(word => <span onClick={() => {searchWord(word)}}>{word}</span>)}
+//                 </p>
+//                 <br></br>
+//             </div>
+//             ))}
+//         </>
+//         )
+//     }
+
+//     //Return Generated Elements
+//     return (
+//         <div style={divstyle} id="scrollbox" onScroll={() => scroll(timer)}>
+//             {renderText(headings, text)}
+//         </div>
+//     )
+// }
